5f8ba8d2d655688a23c7e02ecb40b10847b229d6
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-04-15 22:04:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 function removeFromPlaylist (playlist, artistName) {
- delete [artistName];
+ delete playlist.artistName;
   return playlist
 }
