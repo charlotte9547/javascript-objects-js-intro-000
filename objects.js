fc739de6c791e8f4b43c652bc95a159c6a235b01
Xingxin Chen <xingxinc@andrew.cmu.edu>
2018-04-15 22:00:01 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,8 @@
 var playlist = {beethoven: "Appassionata"};
 
 
-function updatePlayList (playlist, artistName, songTitle){
-playlist [artistName] = songTitle
-return playlist
+function updatePlaylist(playlist, artistName, songTitle) {
+
+  playlist[artistName] = songTitle
+  return playlist
 }
